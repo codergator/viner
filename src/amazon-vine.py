4325554bb0a2216f1f3394b5a7c81852edf8d3a1
The Codergator <codergator@gmail.com>
2019-03-06 19:52:08 -0800

    Refactor (#2)
    * remove Poetry config
    
    * refactor: Final constants

--- src/amazon-vine.py
+++ src/amazon-vine.py
@@ -1,23 +1,8 @@
-# Copyright 2014, Timur Tabi
-# Copyright 2019, The Codergator
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import sys
 import re
 import time
 from typing import Set
+from typing_extensions import Final
 import urllib.request
 import urllib.error
 import urllib.parse
@@ -35,46 +20,36 @@ import getpass
 from optparse import OptionParser
 
 
-def user_agent() -> str:
-    return fake_useragent.UserAgent().ff
+INITIAL_PAGE: Final = 'https://www.amazon.com/gp/vine/'
+QUEUE_URL: Final = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_Default'
+VFA_URL: Final = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_LastChance'
+USER_AGENT: Final[str] = fake_useragent.UserAgent().ff
 
 
-QUEUE_URL = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_Default'
-VFA_URL = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_LastChance'
-
-
-def create_browser(user_agent) -> mechanize.Browser:
-    global options
-
-    br = mechanize.Browser()
-
-    # Load cookies from the selected web browser
-    cj2 = getattr(browsercookie, options.browser)()
+def create_browser() -> mechanize.Browser:
+    browser = mechanize.Browser()
+    firefox = getattr(browsercookie, OPTIONS.browser)()
 
     # Create a new cookie jar for Mechanize
     cj = http.cookiejar.CookieJar()
-    for cookie in cj2.get_cookies():
+    for cookie in firefox.get_cookies():
         cj.set_cookie(cookie)
-
-    # Load those cookies into mechanize for the session
-    br.set_cookiejar(cj)
+    browser.set_cookiejar(cj)
 
     # Necessary for Amazon.com
-    br.set_handle_robots(False)
-    br.addheaders = [('User-agent', user_agent)]
+    browser.set_handle_robots(False)
+    browser.addheaders = [('User-agent', USER_AGENT)]
 
     try:
-        print('Connecting to vine.amazon.com...')
-        response = br.open('https://www.amazon.com/gp/vine/')
-        html = response.read()
+        print('Connecting...')
+        html = browser.open(INITIAL_PAGE).read()
 
         # Are we already logged in?
         if b'The Exclusive Club of Influential Amazon Voices.' in html:
-            return br
-        else:
-            print('Could not log in with a cookie')
-            sys.exit(1)
+            return browser
 
+        print('Could not log in with a cookie')
+        sys.exit(1)
     except urllib.error.HTTPError as e:
         print(e)
     except urllib.error.URLError as e:
@@ -93,15 +68,16 @@ def download_vine_page(br, url, name=None):
 
     if name:
         print('  Downloading...')
+
     html = response.read()
+
     if name:
         print('  Parsing...')
+
     return bs4.BeautifulSoup(html, features="lxml")
 
 
 def get_list(br, url, name) -> Set[str]:
-    global options
-
     soup = download_vine_page(br, url, name)
     if not soup:
         raise
@@ -131,9 +107,7 @@ def get_list(br, url, name) -> Set[str]:
     return asins
 
 
-def open_vine_page(br, link, url) -> bool:
-    global options
-
+def open_product_page(br, link, url) -> bool:
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), end=' ')
     soup = download_vine_page(br, url % link)
     # Make sure we don't get a 404 or some other error
@@ -164,11 +138,9 @@ parser.add_option('--browser', dest='browser',
                   help='Which browser to use ("firefox" or "chrome") from which to load the session cookies (default is "%default")',
                   type="string", default='firefox')
 
-(options, args) = parser.parse_args()
+(OPTIONS, _args) = parser.parse_args()
 
-
-USER_AGENT = user_agent()
-BROWSER = create_browser(USER_AGENT)
+BROWSER: Final = create_browser()
 
 your_queue_list = get_list(BROWSER, QUEUE_URL, "your queue")
 vine_for_all_list = get_list(BROWSER, VFA_URL, "Vine for All")
@@ -180,14 +152,14 @@ if not vine_for_all_list:
 
 while True:
     print("\nWaiting %u minute%s" %
-          (options.wait, 's'[options.wait == 1:]) + "\n")
-    time.sleep(options.wait * 60)
+          (OPTIONS.wait, 's'[OPTIONS.wait == 1:]) + "\n")
+    time.sleep(OPTIONS.wait * 60)
 
     your_queue_list2 = get_list(BROWSER, QUEUE_URL, "Your Queue")
     if your_queue_list2:
         for link in your_queue_list2.copy():
             if link not in your_queue_list:
-                if not open_vine_page(BROWSER, link, 'https://www.amazon.com/gp/vine/product?ie=UTF8&asin=%s&tab=US_Default'):
+                if not open_product_page(BROWSER, link, 'https://www.amazon.com/gp/vine/product?ie=UTF8&asin=%s&tab=US_Default'):
                     # If the item can't be opened, it might be because the web site
                     # isn't ready to show it to me yet.  Remove it from our list so
                     # that it appears again as a new item, and we'll try again.
@@ -202,7 +174,7 @@ while True:
     if vine_for_all_list2:
         for link in vine_for_all_list2.copy():
             if link not in vine_for_all_list:
-                if not open_vine_page(BROWSER, link, 'https://www.amazon.com/gp/vine/product?ie=UTF8&asin=%s&tab=US_LastChance'):
+                if not open_product_page(BROWSER, link, 'https://www.amazon.com/gp/vine/product?ie=UTF8&asin=%s&tab=US_LastChance'):
                     # If the item can't be opened, it might be because the web site
                     # isn't ready to show it to me yet.  Remove it from our list so
                     # that it appears again as a new item, and we'll try again.
